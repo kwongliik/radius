28caef61038d6b26f48eb1cb202846647a6a7438
david hu <kwongliik@yahoo.com>
2023-05-10 22:13:49 +0800

    added the comments

--- radius2.py
+++ radius2.py
@@ -1,12 +1,15 @@
+# function to read input
 def read_input(): 
     r = float(input("key in radius: "))
     return r
 
+# function to calculate the area of circle
 def calculate(r):
     pi = 3.142
     area = pi * r ** 2
     return area
 
+# main
 def main():
     r = read_input()
     area = calculate(r)
